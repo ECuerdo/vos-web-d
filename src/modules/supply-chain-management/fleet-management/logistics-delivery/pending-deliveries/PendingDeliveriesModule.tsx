47181f7c7b45e529772c75430960b37b3959b326
EuLo C. <eulysiscuerdo03@gmail.com>
2026-05-22 11:16:30 +0800

    Eulysis branch (#7)
    * Update
    
    * Update 5/18/2026
    
    Update FM and SCM
    
    * Update 5/21/2026 3:51pm
    
    * Create ci.yml
    
    * Update CustomerDiscountFilters.tsx
    
    * Update ci.yml
    
    * Upadate 5/2/2026 11:08am

--- src/modules/supply-chain-management/fleet-management/logistics-delivery/pending-deliveries/PendingDeliveriesModule.tsx
+++ src/modules/supply-chain-management/fleet-management/logistics-delivery/pending-deliveries/PendingDeliveriesModule.tsx
@@ -414,7 +414,8 @@ export default function PendingDeliveriesModule() {
             .sort((a, b) => a[0].localeCompare(b[0]))
             .map(([name, total]) => [name, formatNumberForPDF(total)]);
 
-        let finalY = (doc as jsPDF & { lastAutoTable?: { finalY: number } }).lastAutoTable?.finalY || 50;
+        // @ts-expect-error - jsPDF autotable internal state access
+        let finalY = doc.lastAutoTable?.finalY || 50;
         if (finalY > 160) { doc.addPage(); finalY = 20; } else { finalY += 10; }
 
         doc.setFontSize(10);
